d0db12e0ccd96cebda2cf24c7c9331cde85d3c93
Arokota <williamsj725@mail.com>
2021-07-12 00:54:00 -0400

    Fixing API issues and frontend

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from python_terraform import *
 
@@ -11,7 +12,17 @@ class MHat(BaseModel):
     
 app = FastAPI()
 
-@app.post("/http-redirector/{mhat_id}")
+
+#Added this to get rid of the CORS errors blocking cross origin traffic
+#https://fastapi.tiangolo.com/tutorial/middleware/
+#https://fastapi.tiangolo.com/tutorial/cors/
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/api/{mhat_id}")
 def CreateMHat(mhat_id: str, mhat_request: MHat):
     t = Terraform(working_dir='modules/http-redirector')
     t.init()
@@ -21,18 +32,18 @@ def CreateMHat(mhat_id: str, mhat_request: MHat):
     return_code, stdout, stderr = t.apply(**{"skip_plan": True, "auto_approve": IsFlagged, "capture_output": True}, variables=mhat_request_dict)
 
     if return_code == 0:
-        mhat_response_dict = {"Success": True}
+        mhat_response_dict = {"Status": "Success"}
         script_output = t.output(json=True)
         i = 0
         for ip in script_output['public-ip']['value']:
             mhat_response_dict.update({f"mhat_ip{i}": ip})
             i = i + 1
     else:
-        mhat_response_dict = {"Success": False}
+        mhat_response_dict = {"Status": "Failure"}
 
     return mhat_response_dict
 
-@app.delete("/http-redirector/{mhat_id}")
+@app.delete("/api/{mhat_id}")
 def DeleteMHat(mhat_id: str, mhat_request: MHat):
     t = Terraform(working_dir='modules/http-redirector')
     t.init()
@@ -42,22 +53,22 @@ def DeleteMHat(mhat_id: str, mhat_request: MHat):
     return_code, stdout, stderr = t.apply(**{"skip_plan": True, "auto_approve": IsFlagged, "capture_output": True, "destroy": True}, variables=mhat_request_dict)
 
     if return_code == 0:
-        mhat_response_dict = {"Success": True}
+        mhat_response_dict = {"Status": "Success"}
     else:
-        mhat_response_dict = {"Success": False}
+        mhat_response_dict = {"Status": "Failure"}
 
     return mhat_response_dict
 
-@app.get("/http-redirector/{mhat_id}")
+@app.get("/api/{mhat_id}")
 def UpdateMHat(mhat_id: str):
     t = Terraform(working_dir="modules/http-redirector")
     t.init()
     script_output = t.output(json=True)
     #mhat_response_dict = script_output
     if script_output:
-        mhat_response_dict = {"Success": True}
+        mhat_response_dict = {"Status": "Success"}
         
     else:
-        mhat_response_dict = {"Success": False}
+        mhat_response_dict = {"Status": "Failure"}
 
     return mhat_response_dict
